b57c4501cc0591def2e656be08473c5ada408481
tamunosikit2022-a1ly <tamunosikit2022@gmail.com>
2026-05-27 08:44:12 +0100

    Fix emoji encoding in KnowledgeVault

--- src/pages/KnowledgeVault.js
+++ src/pages/KnowledgeVault.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+﻿import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../utils/api";
 import VaultPDFDownloads from "../components/VaultPDFDownloads";
